83bbe669ccc464254a9d3545ab3fdbd061cc342b
aleksanderwalczuk <ohleg@windowslive.com>
2023-04-19 22:16:58 +0200

    chore: remove foundation and jquery

--- frontend/app.vue
+++ frontend/app.vue
@@ -8,8 +8,6 @@
 
 <script>
 export default {
-  mounted() {
-    $(document).foundation();
-  },
+
 };
 </script>
--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,3 @@
-import $ from "jquery";
-window.$ = $;
-
-// Foundation (NOTE: Adjust settings in _foundation-settings.scss)
-import "/foundation/foundation.js";
-import "/foundation/foundation.scss";
-import "motion-ui";
-//import "motion-ui/dist/motion-ui.js";
-
-// Application specific.
 import App from "/app.vue";
 import router from "/router";
 import titleMixin from "/mixins/titleMixin";
