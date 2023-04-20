f2da9ab134b90dd48466d5ab030508418ac5ac50
filipkustoch <filip.kustoch1@gmail.com>
2023-04-20 14:37:39 +0200

    Login system

---
+++ backend/classes/dbh.classes.php
@@ -0,0 +1,17 @@
+<?php
+
+class Dbh
+{
+    protected function connect()
+    {
+        try {
+            $username = "root";
+            $password = "";
+            $dbh = new PDO('mysql:host=localhost;dbname=ooplogin', $username, $password);
+            return $dbh;
+        } catch (PDOException $e) {
+            echo "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+}
