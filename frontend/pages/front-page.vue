7d8f8a6262e5dfb4d4a389b440616dc8ce83dcc1
aleksanderwalczuk <ohleg@windowslive.com>
2023-04-29 12:04:09 +0200

    chore: apply code formattins

--- frontend/pages/front-page.vue
+++ frontend/pages/front-page.vue
@@ -1,22 +1,17 @@
- <template>
+<template>
   <register-page />
-  <menu-page />   
+  <menu-page />
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
-import Register from './Register/Register.vue'
-import Menu from './Menu/Menu.vue'
-
+import { defineComponent } from "vue";
+import Register from "./Register/Register.vue";
+import Menu from "./Menu/Menu.vue";
 
 export default defineComponent({
-    components: {
-        'register-page': Register,
-        'menu-page': Menu
-    },
-
-    setup() {
-        return {}
-    },
-})
+  components: {
+    "register-page": Register,
+    "menu-page": Menu,
+  },
+});
 </script>
