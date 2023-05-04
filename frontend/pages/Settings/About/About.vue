bff57bb45177dd30867439b411d17e38b250c390
aniaska4 <aniaska4@wp.pl>
2023-05-04 22:58:48 +0200

    setup settings structure

---
+++ frontend/pages/Settings/About/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>About</div>
+</template>
+    
+<script>
+  import { ref } from 'vue';
+  
+  export default {
+    components: {
+    },
+
+    setup() {
+      return {
+
+      };
+    }
+    };
+</script>
+    
--- frontend/pages/front-page.vue
+++ frontend/pages/front-page.vue
@@ -1,17 +1,21 @@
 <template>
   <register-page />
-  <menu-page />
+  <!-- @TODO temporary false -->
+  <menu-page v-if="false"/>
+  <settings-page />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Register from "./Register/Register.vue";
 import Menu from "./Menu/Menu.vue";
+import Settings from "./Settings/Settings.vue"
 
 export default defineComponent({
   components: {
     "register-page": Register,
     "menu-page": Menu,
+    "settings-page": Settings
   },
 });
 </script>
