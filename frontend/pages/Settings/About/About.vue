87db7c93a5975496e624f05aee0ada8c4848376a
Jelena <jelena.agiejewa@gmail.com>
2023-05-22 11:49:23 +0200

    fixes after code review

--- frontend/pages/Settings/About/About.vue
+++ frontend/pages/Settings/About/About.vue
@@ -1,154 +1,60 @@
-<style>
-			* {
-				margin: 0;
-				padding: 0;
-				box-sizing: border-box;
-			}
-
-			.grid {
-				display: grid;
-				font-family: 'sans-serif';
-				background-color: rgb(15 23 42);
-				/* color: rgb(249, 115, 22); */
-			}
-
-			.customer-data {
-				padding: 30px 0;
-				/* text-align: center; */
-				font-size: 26px;
-				/* text-transform: uppercase; */
-				letter-spacing: 2px;
-			}
-
-			.grid-date {
-				display: grid;
-				grid-template: 1fr 1fr / minmax(50px, 90px) auto;
-				justify-items: start;
-				align-items: center;
-				row-gap: 25px;
-				margin: 0 auto;
-			}
-
-			.input-btn {
-				padding: 8px 16px;
-				border-radius: 40px;
-				border-width: 0px;
-				font-size: 100%;
-			}
-
-			.bg-c {
-				background-color: rgba(30, 41, 59, 0.502);
-			}
-
-			.flex {
-				/* display: flex; */
-				column-gap: 4px;
-			}
-
-			.toggle {
-				position: relative;
-			}
-
-			.toggle input[type='checkbox'] {
-				position: absolute;
-				left: 0;
-				top: 0;
-				width: 100%;
-				height: 100%;
-				opacity: 0;
-				z-index: 5;
-				cursor: pointer;
-			}
-
-			.toggle label {
-				display: flex;
-				position: relative;
-				align-items: center;
-			}
-
-			.toggle label:before {
-				content: '';
-				position: relative;
-				display: inline-block;
-				width: 40px;
-				height: 20px;
-				background-color: rgb(249, 115, 22);
-				border: 2px solid #e2e2e2;
-				box-shadow: inset -20px 0 0 #e2e2e2;
-				border-radius: 20px;
-				box-sizing: border-box;
-				transition: 0.25s ease-in-out;
-			}
-
-			.toggle input[type='checkbox']:checked + label:before {
-				box-shadow: inset 20px 0 0 rgb(99, 102, 241);
-			}
-
-			.flex .left {
-				margin-left: 12px;
-			}
-
-			.btn-submit {
-				grid-column: 1 / 3;
-				margin-right: 40px;
-				justify-self: end;
-				color: white;
-				background-color: rgb(249, 115, 22);
-				/* text-transform: uppercase; */
-				cursor: pointer;
-			}
-
-			.btn-submit:hover {
-				background-color: rgb(99, 102, 241);
-			}
-		</style>
-
 <template>
- <section class="grid text-orange-400">
-			<h2 class="customer-data uppercase text-center">Profile</h2>
-			<form action="" method="post" class="grid-date">
-				<p>Name:</p>
+<section class="grid place-content-center bg-slate-900">
+			<h2
+				class="uppercase m-6 text-center text-3xl font-bold tracking-widest text-orange-400">
+				Profile
+			</h2>
+			<form action="" method="post" class="grid place-items-center gap-6">
+				<p class="text-orange-400">Name:</p>
 				<input
 					v-model="form.name"
 					type="text"
 					name="uid"
 					placeholder="Name"
-					class="input-btn bg-c" />
-				<p>Surname:</p>
+					class="w-64 appearance-none rounded-full border-0 bg-slate-800/50 p-2 px-4 text-slate-300 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
+				<p class="text-orange-400">Surname:</p>
 				<input
 					v-model="form.surname"
 					type="text"
 					name="uid"
 					placeholder="Surname"
-					class="input-btn bg-c" />
-				<p>Gender:</p>
-				<section class="flex">
-					<p>male:</p>
-					<div class="toggle">
-						<input type="checkbox" />
-						<label></label>
+					class="w-64 appearance-none rounded-full border-0 bg-slate-800/50 p-2 px-4 text-slate-300 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
+				<p class="text-orange-400">Gender:</p>
+				<section class="flex gap-14">
+					<div>
+						<p class="inline-block mr-2
+            text-orange-400">male:</p>
+						<div class="toggle relative inline-block">
+							<input type="checkbox" class="absolute left-0 top-2 w-full h-full opacity-0 z-10 cursor-pointer"/>
+							<label class="flex relative items-center"></label>
+						</div>
 					</div>
-					<p class="left">female:</p>
-					<div class="toggle">
-						<input type="checkbox" />
-						<label></label>
+					<div>
+						<p class="inline-block mr-2 text-orange-400">female:</p>
+						<div class="toggle relative inline-block">
+							<input type="checkbox" class="absolute left-0 top-2 w-full h-full opacity-0 z-10 cursor-pointer"/>
+							<label class="flex relative items-center"></label>
+						</div>
 					</div>
 				</section>
-				<p>Weight:</p>
+				<p class="text-orange-400">Weight:</p>
 				<input
 					v-model="form.weight"
 					type="text"
 					name="uid"
 					placeholder="Weight"
-					class="input-btn bg-c" />
-				<p>Height:</p>
+					class="w-64 appearance-none rounded-full border-0 bg-slate-800/50 p-2 px-4 text-slate-300 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
+				<p class="text-orange-400">Height:</p>
 				<input
 					v-model="form.height"
 					type="text"
 					name="uid"
 					placeholder="Height"
-					class="input-btn bg-c" />
-				<button type="submit" class="btn-submit input-btn uppercase" @click="submit">
+					class="w-64 appearance-none rounded-full border-0 bg-slate-800/50 p-2 px-4 text-slate-300 focus:bg-slate-800 focus:ring-2 focus:ring-orange-500" />
+				<button
+					type="submit"
+					class="btn-submit col-span-2 justify-self-end mr-5 rounded-full bg-orange-500 p-2 px-4 text-white uppercase hover:bg-indigo-500"
+					@click="submit">
 					Submit
 				</button>
 			</form>
@@ -169,3 +75,24 @@
     }
     };
 </script>
+<style>
+			.toggle label:before {
+				content: '';
+				position: relative;
+                top: 2px;
+				display: inline-block;
+				width: 40px;
+				height: 20px;
+				background-color: rgb(249, 115, 22);
+				border: 2px solid #e2e2e2;
+				box-shadow: inset -20px 0 0 #e2e2e2;
+				border-radius: 20px;
+				box-sizing: border-box;
+				transition: 0.25s ease-in-out;
+			}
+
+			.toggle input[type='checkbox']:checked + label:before {
+				box-shadow: inset 20px 0 0 rgb(99, 102, 241);
+			}
+				</style>
+				
